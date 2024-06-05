706322edf222428f3fc34f371ba794f0e7c4b55a
Albert NIYONSENGA <94793290+AlbertNIYONSENGA@users.noreply.github.com>
2024-06-05 22:34:29 +0200

    done Update challenge1.py

--- challenge1.py
+++ challenge1.py
@@ -2,6 +2,21 @@
 
 def football_points(wins, draws, losses):
     # Write your solution here
+    print('wins: ',end='')
+    wins=int(input())
+    print('Draws: ',end='')
+    draws=int(input())
+    print('Losses: ',end='')
+    losses=int(input())
+    if wins>=0 and draws>=0 and losses>=0:
+    #logic behind
+      wins=wins*3
+      draws=draws*1
+      losses=losses*0
+      points=wins+draws+losses
+      return points
+     else:
+      print('Input must be greater or equal to 0')
     pass
 
 # Test cases
@@ -14,6 +29,22 @@ print(football_points(0, 0, 1))  # Output should be 0
 
 def chatroom_status(users):
     # Write your solution here
+    """seems that this logic is so tricky,i just thought that am gonna need the input
+    Till i found that i got the value in the user list
+    i was about to die thinking about input wallah
+    """
+    #users=[]- no need to make this list
+    if len(users)==0:
+        return('No one online')
+
+    elif len(users)==1:
+        return(users[0]+' online')
+
+    elif len(users)==2:
+        return(users[0]+' and '+users[1]+' online')
+
+    elif len(users)>2:
+        return(users[0]+' , '+users[1] +' and '+str(len(users)-2)+' more online')
     pass
 
 # Test cases
@@ -27,6 +58,11 @@ print(chatroom_status(["pap_ier44", "townieBOY", "panda321", "motor_bike5", "san
 
 def is_valid_PIN(pin):
     # Write your solution here
+     if len(pin)==4:
+       return(pin.isdecimal())
+     else: #here indentation error made it somehow complicated
+       return(pin.isalpha()) 
+    
     pass
 
 # Test cases
