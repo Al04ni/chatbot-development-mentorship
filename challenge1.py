612672baa76ec64f9e184d4d2c2c2c7df79fabba
Edison UWIHANGANYE <116379485+codeWithEdison@users.noreply.github.com>
2024-06-05 11:45:57 +0200

    Update and rename football_points.py to challenge1.py

---
+++ challenge1.py
@@ -0,0 +1,25 @@
+# 1football_points task
+
+def football_points(wins, draws, losses):
+    # Write your solution here
+    pass
+
+# Test cases
+print(football_points(3, 4, 2))  # Output should be 13
+print(football_points(5, 0, 2))  # Output should be 15
+print(football_points(0, 0, 1))  # Output should be 0
+
+
+# 2.chatroom_status task
+
+def chatroom_status(users):
+    # Write your solution here
+    pass
+
+# Test cases
+print(chatroom_status([]))  # Output should be "no one online"
+print(chatroom_status(["paRIE_to"]))  # Output should be "paRIE_to online"
+print(chatroom_status(["s234f", "mailbox2"]))  # Output should be "s234f and mailbox2 online"
+print(chatroom_status(["pap_ier44", "townieBOY", "panda321", "motor_bike5", "sandwichmaker833", "violinist91"]))
+# Output should be "pap_ier44, townieBOY and 4 more online"
+
